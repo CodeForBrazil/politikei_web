cfe333b87a8c55f7f2e66ebc42d9c77da94f1f74
Thiago Classen <tgclassen.dev@gmail.com>
2016-08-19 01:44:36 -0300

    -created login button and modal.

--- app/src/app.js
+++ app/src/app.js
@@ -1,7 +1,7 @@
 function config($locationProvider, $stateProvider, $urlRouterProvider, $mdThemingProvider, $mdIconProvider) {
     
-    //$locationProvider.html5Mode(true);
-    //$urlRouterProvider.otherwise("/");
+    $locationProvider.html5Mode(true);
+    $urlRouterProvider.otherwise("/");
 
     $stateProvider
         .state('public', {
@@ -73,9 +73,9 @@ var politikei = angular
     .config(config)
     .controller('MainController', MainController);
 
-var config = {
+var configEnd = {
     token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjIwMiwiaXNzIjoiaHR0cDpcL1wvcG9saXRpa2VpLWFwaS5oZXJva3VhcHAuY29tXC9hcGlcL3YxXC9hdXRoXC9mYWNlYm9vayIsImlhdCI6MTQ1NTczMjM2MiwiZXhwIjoxNDU1NzM1OTYyLCJuYmYiOjE0NTU3MzIzNjIsImp0aSI6IjU5MDI5MTU2MDE4MTJlYzQ0OGNmMzBmZmIxNmQ2ZGU1In0.-D5T4f2eA-l_fmKvave0n7djKihKMVgoG44GmG8scZM',
     server: 'http://politikei-api-v5.herokuapp.com/'
 };
 
-politikei.constant('app_config', config);
+politikei.constant('app_config', configEnd);
